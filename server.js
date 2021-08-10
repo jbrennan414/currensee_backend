e50d21278bb2471a0c732a309e0ed22ed35e105f
John Brennan <jbrennan@fluentstream.com>
2021-08-09 20:47:09 -0600

    refactor port numbers

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/', async (req, res) => {
     res.send("Hello world")
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     
     console.log(`Server listening on port ${PORT}`);
