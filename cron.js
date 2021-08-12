62550691e32ea48b141d7a4b805fb178f850f7cd
John Brennan <jbrennan@fluentstream.com>
2021-08-12 07:49:12 -0600

    cron updates

---
+++ cron.js
@@ -0,0 +1,6 @@
+var CronJob = require('cron').CronJob;
+
+const addExchangeRatesToRedis = require('./addExchangeRatesToRedis');
+
+// Every hour from 6 am - 10 pm
+new CronJob('* * * * *', addExchangeRatesToRedis, null, true, "America/Denver");
--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 // server.js
 const express = require('express');
 const app = express();
-const { addExchangeRatesToRedis } = require('./addExchangeRatesToRedis');
 const { getRatesFromRedis } = require('./getRatesFromRedis');
 require('dotenv').config();
-
-
-app.get('/seed_data', async (req, res) => {
-    let exchangeRates = await addExchangeRatesToRedis(true)
-    res.send(exchangeRates);
-});
 
 app.get('/rates', async (req, res) => {
     let exchangeRates = await getRatesFromRedis()
