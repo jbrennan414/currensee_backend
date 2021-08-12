c35c4b7dde308d7a3be1c476129113c6439ff883
John Brennan <jbrennan@fluentstream.com>
2021-08-12 08:40:28 -0600

    change cron to every 12 hours

--- cron.js
+++ cron.js
@@ -3,4 +3,4 @@ var CronJob = require('cron').CronJob;
 const { addExchangeRatesToRedis } = require('./addExchangeRatesToRedis');
 
 // Every hour from 6 am - 10 pm
-new CronJob('* * * * *', addExchangeRatesToRedis, null, true, "America/Denver");
+new CronJob('0 */12 * * *', addExchangeRatesToRedis, null, true, "America/Denver");
