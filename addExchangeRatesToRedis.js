b13727783acbc7d25f55ff2949a0a8afd4b5c0c1
John Brennan <jbrennan@fluentstream.com>
2021-08-12 08:34:52 -0600

    only prod options

--- addExchangeRatesToRedis.js
+++ addExchangeRatesToRedis.js
@@ -1,27 +1,28 @@
 var redis = require('redis'), client = redis.createClient();
 const { fetchExchangeRates } = require('./fetchExchangeRates');
 
-async function addExchangeRatesToRedis(isProduction){
+async function addExchangeRatesToRedis(){
+
+    console.log("fetching exchange rates...")
     
-    if (isProduction) {
+    // if (isProduction) {
         let exchangeRates = await fetchExchangeRates()
         console.log(`Setting fresh exchange rates to redis at ${exchangeRates.timestamp}`)
         client.HMSET("rates", exchangeRates["rates"])
         client.set("timestamp", exchangeRates["timestamp"])
         return "OK";
 
-    } else {
+    // } else {
 
-        const fs = require('fs');
+        // const fs = require('fs');
 
-        let rawdata = fs.readFileSync('sample_response.json');
-        let sampleExchangeRates = JSON.parse(rawdata);
-        console.log(`Setting TEST EXCHANGE RATES to redis...this better not be production!`)
-        client.HMSET("rates", sampleExchangeRates["rates"])
-        client.set("timestamp", sampleExchangeRates["timestamp"])
-        return "OK";
-        
-    }
+        // let rawdata = fs.readFileSync('sample_response.json');
+        // let sampleExchangeRates = JSON.parse(rawdata);
+        // console.log(`Setting TEST EXCHANGE RATES to redis...this better not be production!`)
+        // client.HMSET("rates", sampleExchangeRates["rates"])
+        // client.set("timestamp", sampleExchangeRates["timestamp"])
+        // return "OK";
+    // }
 }
 
 module.exports = {addExchangeRatesToRedis: addExchangeRatesToRedis};
