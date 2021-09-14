57e34435b6130b2b62ffb8a576e2cab6cacb4292
John Brennan <jbrennan@fluentstream.com>
2021-09-13 21:48:43 -0600

    add an html site

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cron = require('node-cron')
 const app = express();
 const { getRatesFromRedis } = require('./getRatesFromRedis');
 const { addExchangeRatesToRedis } = require('./addExchangeRatesToRedis');
+const fs = require("fs")
+
 require('dotenv').config();
 
 cron.schedule('0 */6 * * *', function() {
@@ -14,6 +16,18 @@ app.get('/rates', async (req, res) => {
     let exchangeRates = await getRatesFromRedis()
     res.send(exchangeRates);
 });
+
+app.get("/", (req, res) => {
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            res.writeHead(404);
+            res.write("Whoops, file not found")
+        } else {
+            res.write(data);
+        }
+        res.end()
+    })
+})
 
 const PORT = 5000;
 app.listen(PORT, () => {
