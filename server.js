9e2daf87b57f092cd83b27aae1a5d60ee8ccd495
John Brennan <brennanj414@gmail.com>
2021-10-13 21:36:54 -0600

    add datadog logger

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const { addExchangeRatesToRedis } = require('./addExchangeRatesToRedis');
 const fs = require("fs")
 var StatsD = require('hot-shots');
 var dogstatsd = new StatsD();
+const { logger } = require("./logger");
 
 require('dotenv').config();
 
@@ -17,12 +18,14 @@ cron.schedule('0 */6 * * *', function() {
 app.get('/rates', async (req, res) => {
     let exchangeRates = await getRatesFromRedis()
     dogstatsd.increment('page.views')
+    logger.info("Someone requested rates")
     res.send(exchangeRates);
 });
 
 app.get("/", (req, res) => {
     fs.readFile('./index.html', null, function (error, data) {
         if (error) {
+            logger.error("index.html error", error.message)
             res.writeHead(404);
             res.write("Whoops, file not found")
         } else {
@@ -35,6 +38,7 @@ app.get("/", (req, res) => {
 app.get("/privacy", (req, res) => {
     fs.readFile('./privacy.html', null, function (error, data) {
         if (error) {
+            logger.error("privacy.html error", error.message)
             res.writeHead(404);
             res.write("Whoops, file not found")
         } else {
